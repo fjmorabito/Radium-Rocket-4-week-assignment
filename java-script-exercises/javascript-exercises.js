45d12dca94a88dbbf366cdd2a1dd1a4a912b62e0
Federico Morabito <morabitofj@gmail.com>
2020-10-27 18:30:15 -0300

    PS-04: If Else exercises added

--- java-script-exercises/javascript-exercises.js
+++ java-script-exercises/javascript-exercises.js
@@ -105,3 +105,42 @@ month.join("-");
 var month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre",
 "Noviembre", "Diciembre"];
 var month2 = month.slice(4 , 11);
+
+//                                                If Else
+
+/* part a) create a random number between 0 and 1 using the function Math.random(), if the value is greater or equal than 0,5, 
+show an alert with a message "Greater than 0,5", but an alert with a message "Lower than 0.5"  */
+
+var randomNumber = Math.random(0 , 1);
+if (randomNumber >= 0.5) {
+    alert("Greater than 0,5" , randomNumber);
+} else {
+    alert ("Lower than 0,5" , randomNumber);
+}
+
+/* part b) Create an "Age" variable that contains an integer between 0 and 100 and displays the following alert messages:
+"Bebé" if the age is less than 2 years
+"Niño" if the age is between 2 and 12 years
+"Adolescente" if the age is between 13 and 19 years
+"Joven" if the age is between 20 and 30 years
+"Adulto" between 31 and 60 years
+“Adulto mayor” between 61 and 75 years
+"Anciano" if older than 75 years*/
+
+var age = Math.round(Math.random()*100);
+
+if(age < 2) {
+    alert("Bebe")
+} else if (age <= 12) {
+    alert("Niño")
+}else if (age <= 19) {
+    alert("Adolocente")
+}else if (age <= 30) {
+    alert("Joven")
+}else if (age <=60) {
+    alert("Adulto")
+}else if (age <=75) {
+    alert("Adulto mayor")
+} else {
+    alert("Anciano") 
+}
