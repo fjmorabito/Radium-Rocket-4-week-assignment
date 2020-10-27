7597a5cb6f672be7505930ab44eaf1d3bbf761c4
Federico Morabito <morabitofj@gmail.com>
2020-10-27 18:24:35 -0300

    PS-04: variables and parameters exercises added

---
+++ java-script-exercises/javascript-exercises.js
@@ -0,0 +1,21 @@
+//                                                Variable and operators                                   
+
+/* part a) create two numerical variables and use the operator sum for save the value of the sum of both numbers on a 3rd 
+variable.*/
+
+var var1 = 5;
+var var2 = 25;
+var var3 = var1 + var2;
+
+/* part b) create two string type variables and concatenate them saving the result on a 3rd variable. */
+
+var string1 = "Federico ";
+var string2 = "Morabito";
+var string3 = string1 + string2;
+
+/* part c) create two string type variables and sum the long of each variable saving the result of the sum on a 3rd variable. */
+
+var string1 = "Federico ";
+var string2 = "Morabito";
+var stringlenght = string1.length + string2.length;
+
